d18a45fbc85bea6e87c36df309a8fd56192d0376
Stefanie Dao <ctdao@ucsd.edu>
2021-11-06 11:43:21 -0700

    part-1b

--- sw.js
+++ sw.js
@@ -12,12 +12,12 @@ self.addEventListener('install', function (event) {
    * TODO - Part 2 Step 2
    * Create a function as outlined above
    */
-   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
+  //  event.waitUntil(
+  //   caches.open(CACHE_NAME)
+  //     .then(function(cache) {
+  //       return cache.addAll(urlsToCache);
+  //     })
+  // );
 });
 
 /**
@@ -31,7 +31,7 @@ self.addEventListener('activate', function (event) {
    * Create a function as outlined above, it should be one line
    */
 
-   event.waitUntil(clients.claim());
+  //  event.waitUntil(clients.claim());
 });
 
 // Intercept fetch requests and store them in the cache
@@ -40,31 +40,31 @@ self.addEventListener('fetch', function (event) {
    * TODO - Part 2 Step 4
    * Create a function as outlined above
    */
-   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
+  //  event.respondWith(
+  //   caches.match(event.request)
+  //     .then(function(response) {
+  //       // Cache hit - return response
+  //       if (response) {
+  //         return response;
+  //       }
 
-        return fetch(event.request).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
+  //       return fetch(event.request).then(
+  //         function(response) {
+  //           // Check if we received a valid response
+  //           if(!response || response.status !== 200 || response.type !== 'basic') {
+  //             return response;
+  //           }
             
-            var responseToCache = response.clone();
+  //           var responseToCache = response.clone();
 
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
+  //           caches.open(CACHE_NAME)
+  //             .then(function(cache) {
+  //               cache.put(event.request, responseToCache);
+  //             });
 
-            return response;
-          }
-        );
-      })
-    );
+  //           return response;
+  //         }
+  //       );
+  //     })
+  //   );
 });
